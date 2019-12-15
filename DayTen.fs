b2b1d9661a3389dc0894e87d7e2855e611288501
Claye_L <claye@ixcys.fr>
2019-12-16 00:47:03 +0100

    work on new angle formula

--- DayTen.fs
+++ DayTen.fs
@@ -69,3 +69,32 @@ let TenOne =
     let (point,visible) = List.map (fun x -> x, visiblePoints x data) data |> List.maxBy (snd >> Set.count )
     printfn "%A" point
     printfn "%d" <| Set.count visible
+
+let optimalPoint = (20,20)
+
+[<StructuralEquality; CustomComparison>]
+type AngleCosine =
+    |R of float
+    |L of float
+    
+    interface System.IComparable with
+        member x.CompareTo yobj =
+            let comp x y =
+                match x,y with
+                |R a, R b -> compare b a
+                |R _, L _ -> -1
+                |L _, R _ -> 1
+                |L a, L b -> compare b a
+            match yobj  with
+            | :? AngleCosine as y -> comp x y
+            | _ -> failwith "unauthorized comparison of AngleCosine object"
+
+let vecWithPoint (a,b) (x,y) =   x - a, y - b
+    
+let cosAngle (x,y) = 
+    let right =  x >= 0
+    let (a,b) = if right then 0,1 else 0,-1
+    let dotproduct = a * x + b * y
+    let normproduct = float 1 + ( (float >>  Math.Sqrt) (x * x + y * y))
+    let cos = float dotproduct / (normproduct)
+    (if right then R else L)  
